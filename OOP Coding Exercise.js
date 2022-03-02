6f24757eaefb911f84881af9f0b028ffaf489013
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-02 12:49:38 +1100

    Constructor Functions and ES6 Classes Exercise

--- OOP Coding Exercise.js
+++ OOP Coding Exercise.js
@@ -47,3 +47,57 @@ bmw.accelerate();
 bmw.accelerate();
 bmw.brake();
 bmw.brake();
+
+/* Coding Challenge 2 
+
+1. Recreate Challenge 1, but using only ES6 classes;
+
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+*/
+
+class CarClass {
+    constructor (make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() { 
+        this.speed += 10;
+        console.log(`${this.make} is going at ${this.speed} km/h`)
+    }
+    
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is going at ${this.speed} km/h`)
+    }
+
+    get speedUS() {
+        return this.speed/1.6;
+    }
+
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+}
+
+//creating new object
+const ford = new CarClass('Ford', 120);
+
+//checking miles
+console.log(ford.speedUS);
+
+//testing methods
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+
+//setting the speed to test km conversion
+ford.speedUS = 50;
+console.log(ford);
