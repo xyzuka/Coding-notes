fcdc8d4fc98fa1e9d6e65de2f6edf6f5a5497626
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-02 19:46:22 +1100

    Update OOP Coding Exercise.js

--- OOP Coding Exercise.js
+++ OOP Coding Exercise.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Coding Challenge OOP
+// Coding Challenge 1 - OOP
 
 /*
 
@@ -48,7 +48,9 @@ bmw.accelerate();
 bmw.brake();
 bmw.brake();
 
-/* Coding Challenge 2 
+/******************** END *********************/
+
+/* Coding Challenge 2 - ES6
 
 1. Recreate Challenge 1, but using only ES6 classes;
 
@@ -101,3 +103,67 @@ ford.brake();
 //setting the speed to test km conversion
 ford.speedUS = 50;
 console.log(ford);
+
+/******************** END *********************/
+
+// Coding Challenge 3 - Constructor Functions
+
+/* 
+1. Use a constructor function to implement an Electric Car (called EV) as a CHILD "class" of Car. Besides a make and current speed, the EV also has the current battery charge in % ('charge' property);
+
+2. Implement a 'chargeBattery' method which takes an argument 'chargeTo' and sets the battery charge to 'chargeTo';
+
+3. Implement an 'accelerate' method that will increase the car's speed by 20, and decrease the charge by 1%. Then log a message like this: 'Tesla going at 140 km/h, with a charge of 22%';
+
+4. Create an electric car object and experiment with calling 'accelerate', 'brake' and 'chargeBattery' (charge to 90%). Notice what happens when you 'accelerate'! HINT: Review the definiton of polymorphism
+
+DATA CAR 1: 'Tesla' going at 120 km/h, with a charge of 23%
+*/
+
+// Goal -> creating a sub class of the Car object and connecting their prototypes
+
+/******************* SOLUTION *******************/
+
+// parent object
+const Car = function (make, currentSpeed) {
+  this.make = make;
+  this.currentSpeed = currentSpeed;
+}
+
+// Accelerate and brake methods
+Car.prototype.accelerate = function() {
+  this.currentSpeed += 20;
+  this.charge--;
+  console.log(`${this.make} is going at ${this.currentSpeed} km/h, with a charge of ${this.charge}%.`);
+}
+
+Car.prototype.brake = function() {
+  this.currentSpeed -= 5;
+  console.log(`${this.make} is going at ${this.currentSpeed} km/h, with a charge of ${this.charge}%.`);
+}
+
+// child object
+const EV = function(make, currentSpeed, charge) {
+  Car.call(this, make, currentSpeed);
+  this.charge = charge;
+}
+
+// linking EV to Car -> to inherit the prototype properties of Car
+EV.prototype = Object.create(Car.prototype)
+
+// Implementing a recharge method
+EV.prototype.chargeBattery = function(chargeTo) {
+  this.charge = 90;
+  console.log(`${this.make} has been recharged, with a charge of ${this.charge}%.`);
+}
+
+const tesla = new EV('Tesla', 120, 23);
+
+tesla.accelerate();
+tesla.brake();
+tesla.chargeBattery();
+
+// Fixing the prototype assignment of EV
+EV.prototype.constructor = EV;
+
+// If the child object has its own method that has the same name as its parent object (eg. Car.prototype.accelerate and EV.prototype.accelerate existing together) , the first method that appears will be run first but then the second method below will override the first method -> thus satisfying the definition of polymorphism (having different variations)
