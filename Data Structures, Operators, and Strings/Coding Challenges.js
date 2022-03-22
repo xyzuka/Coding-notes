c0f5ae5b93f19551991e447967ed874f26385641
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-22 13:49:35 +1100

    Update Coding Challenges.js

--- Data Structures, Operators, and Strings/Coding Challenges.js
+++ Data Structures, Operators, and Strings/Coding Challenges.js
@@ -194,5 +194,62 @@ for (const [team, odd] of Object.entries(game.odds)) {
 }
 
 /////////////////////////////////////////////////////////////////
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1. Create a set from the values of the map
+console.log(gameEvents.values());
+const events = new Set(gameEvents.values());
+console.log(events);
+
+// 2. Deleting event 64
+gameEvents.delete(64);
+console.log(gameEvents);
+
+// 3. Console log average event time
+// Note pop gets the last event time
+const time = [...gameEvents.keys()].pop();
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+// 4. Looping event timing
+for (const [key, event] of gameEvents) {
+  key <= 45
+    ? console.log(`[FIRST HALF]: ${key}: ${event}`)
+    : console.log(`[SECOND HALF]: ${key}: ${event}`);
+}
+
 
 
