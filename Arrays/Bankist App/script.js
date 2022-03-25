b3b8c093da214f82765f7a3be8ca0d87ecab5358
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-25 12:08:47 +1100

    Add files via upload

---
+++ Arrays/Bankist App/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// LOOPING THROUGH MAPS AND SETS
+
+//* MAPS
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+//* calling forEach() on a map
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+// USD: United States dollar
+// EUR: Euro
+// GBP: Pound sterling
+
+//* SETS
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+// USD: USD
+// GBP: GBP
+// EUR: EUR
