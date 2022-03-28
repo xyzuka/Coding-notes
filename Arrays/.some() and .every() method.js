1362df6c22900672822a32fef47590a10ec3a363
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-28 17:02:20 +1100

    Update .some() and .every() method.js

--- Arrays/.some() and .every() method.js
+++ Arrays/.some() and .every() method.js
@@ -7,6 +7,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 console.log(movements.includes(-130)); // true
 console.log(movements.includes(typeof "string")); // false
 
+////////////////////////////////////
+////////////////////////////////////
 // SOME METHOD
 // The some method will test based off a condition instead
 
@@ -18,3 +20,20 @@ console.log(anyDeposits); // true
 const deposits5000 = movements.some((mov) => mov > 5000);
 console.log(deposits5000); // false
 
+////////////////////////////////////
+////////////////////////////////////
+// EVERY METHOD
+// Only returns true if every element in an array satisfies the condition
+
+const deposits = [200, 450, 400, 3000, 650, 130, 70, 1300];
+
+const findWithdraws = deposits.every((mov) => mov < 0);
+console.log(findWithdraws); // false
+
+const depositCheck = deposits.every((mov) => mov > 0);
+console.log(depositCheck); // true
+
+// SEPARATE CALL BACK
+// We can also write a function separately as a variable and pass it as a call back
+const depositChecker = (mov) => mov > 0;
+console.log(deposits.every(depositChecker)); // true
