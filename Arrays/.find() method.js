dd5a9fe3208db1a01547e32e52ec2ae1980f31cf
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-28 11:53:00 +1100

    Create .find() method.js

---
+++ Arrays/.find() method.js
@@ -0,0 +1,45 @@
+// FIND METHOD
+// Retrieves one element from an array based on a condition
+// Does not return a new array, returns only the first element which satisfies the condition
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const firstWithdrawal = movements.find((mov) => mov < 0);
+console.log(firstWithdrawal); // -400
+
+// Filter returns ALL conditions, whereas find returns only ONE element
+
+// Example 2: Working with multiple objects - Find the account of the owner "Jessica Davis"
+
+const account1 = {
+  owner: "Jonas Schmedtmann",
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Jessica Davis",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Steven Thomas Williams",
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Sarah Smith",
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+const account = accounts.find((acc) => acc.owner === "Jessica Davis");
+console.log(account); // {owner: 'Jessica Davis', movements: Array(8), interestRate: 1.5, pin: 2222}
