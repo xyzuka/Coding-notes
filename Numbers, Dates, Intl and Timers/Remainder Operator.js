ddc029d9671682cd098640b5f4df7b8aadcc22a0
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-03-30 13:06:52 +1100

    Create Remainder Operator.js

---
+++ Numbers, Dates, Intl and Timers/Remainder Operator.js
@@ -0,0 +1,9 @@
+// THE REMAINDER OPERATOR
+// Returns the remainder
+console.log(5 % 2); // 1
+console.log(8 % 3); // 2
+
+// Can be used for determining odd or even numbers -> 0 is even, 1 is odd;
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(2)); // true
+console.log(isEven(111)); // false
