12c0b3a9bea1653b32975194486dcdd82d0467e8
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-04-05 19:29:31 +1000

    Create Promisifying a Geolocation API.js

---
+++ Asynchronous Programming/Promisifying a Geolocation API.js
@@ -0,0 +1,63 @@
+//* PROMISIFYING THE GEOLOCATION API (From the Mapty App)
+
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+            <img class="country__img" src="${data.flag}"/>
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${data.population}</p>
+                <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+                <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            </div>
+        </article>
+        `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject); // Shorter version
+  });
+};
+
+getPosition().then(pos => console.log(pos));
+
+// Using the coordinates on the function created from Coding Challenge #1
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Problem with geocoding (${response.status})`);
+      return response.json();
+    })
+    .then(data => {
+      console.log(`You are in ${data.state}, ${data.country}`);
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Country not found (${response.status})`);
+      return response.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => {
+      console.log(`Something went wrong: 💥💥💥 ${err}`);
+    });
+};
+
+const btn = document.querySelector('.btn-country');
+
+btn.addEventListener('click', whereAmI);
