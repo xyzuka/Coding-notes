b6dbe83216d6396b32a0325077c407a39cb4f2e3
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-04-06 23:01:12 +1000

    Create Coding Challenge #3.js

---
+++ Asynchronous Programming/Coding Challenge #3.js
@@ -0,0 +1,46 @@
+2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+// Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+const loadNPause = async function () {
+  try {
+    // Load Image 1
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+    // Load Image 2
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(`${err}🔥`);
+  }
+};
+// loadNPause();
+
+// PART 2
+const loadAll = async function (imgArr) {
+  try {
+    const imgs = imgArr.map(async img => await createImage(img));
+    console.log(imgs);
+
+    //* Using Promise.all because it combines an array
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+
+    // Adding parallel class
+    imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(`${err}!!!🔥`);
+  }
+};
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
+
+// PART 2
+// 1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+// 2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+// 3. Check out the 'imgs' array in the console! Is it like you expected?
+// 4. Use a promise combinator function to actually get the images from the array 😉
+// 5. Add the 'parallel' class to all the images (it has some CSS styles).
