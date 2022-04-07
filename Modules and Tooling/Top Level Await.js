7dfcaa9a91bfc260e1514707179f1ca0f26b669f
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-04-07 14:58:42 +1000

    Create Top Level Await.js

---
+++ Modules and Tooling/Top Level Await.js
@@ -0,0 +1,26 @@
+//* TOP LEVEL AWAIT
+console.log(`Start fetching`);
+const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+const data = await res.json();
+console.log(data);
+console.log('Something');
+// * Top level awaits will block the execution of the entire module!
+
+// Example:
+const getLastPost = async function () {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = getLastPost();
+// Calling a async function, will always return a promise - not the data itself
+console.log(lastPost);
+
+// To get the data, we will call the then method to consume the promise - not clean
+lastPost.then(last => console.log(last));
+
+// We can use a cleaner method to obtain the data (await method)
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+
+//*Note: If a module imports another module with a top level await, then the importing module will wait for the imported module to finish the blocking code
